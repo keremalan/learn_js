e14fe3ee54db944571252bd39464543530271fa5
Kerem Alan <kerem.alan@ogr.sakarya.edu.tr>
2022-01-24 00:11:49 +0300

    Add l3

---
+++ l3/script.js
@@ -0,0 +1,15 @@
+let total = 0; let count = 1;
+while(count <= 10) {
+    total += count;
+    count += 1;
+}
+console.log(total);
+
+let total2 = 0;
+let count2 = 1;
+while (count2 <= 5) {
+    total2 += count2;
+    count2 += 1;
+}
+
+console.log(total2);
